2ae50a183084ed4d1d28d2c3af2a0fe911b35ec4
bruno-gds <brunog.damascena@gmail.com>
2024-03-06 21:04:05 -0300

    incluindo path no estoque open feign

--- src/main/java/com/grupo16/pedidoservice/gateway/http/estoque/EstoqueServiceFeignClient.java
+++ src/main/java/com/grupo16/pedidoservice/gateway/http/estoque/EstoqueServiceFeignClient.java
@@ -5,13 +5,12 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.grupo16.pedidoservice.gateway.http.estoque.json.EstoqueJson;
 
-@FeignClient(value = "ESTOQUE-SERVICE")
+@FeignClient(value = "ESTOQUE-SERVICE", path = "estoques")
 public interface EstoqueServiceFeignClient {
 	
 	@GetMapping("{idsProdutos}")
